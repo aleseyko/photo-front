9e63c833ad351e538a5f5da1adc2767ef287f55b
Oleksii Bondar <aleseyko@gmail.com>
2019-09-14 15:34:40 +0300

    Continue work on drawer

--- src/store.js
+++ src/store.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {SET_CURRENT_LOCATION} from "./mutations";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    currentLocation: {name: 'Долина троянд', description: 'Наначі говоирть фотографуйся тут', mainPhoto: 'src'}
+    currentLocation: {name: 'Долина троянд', description: 'Наначі говоирть фотографуйся тут', mainPhoto: 'src'},
+    locations: [],
   },
   mutations: {
-
+    [SET_CURRENT_LOCATION](state, location) {
+      state.currentLocation = location;
+    },
   },
   actions: {
-
   },
   getters: {
     currentLocation(state) {
       return state.currentLocation
-    }
+    },
+    locations(state) {
+      return state.locations
+    },
   }
 })
