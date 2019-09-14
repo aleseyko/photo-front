8e1507953c4dad301441b78448cbffbc884d3839
VKopachev <kvska@meta.ua>
2019-09-14 12:19:02 +0300

    initial

---
+++ src/components/Map.vue
@@ -0,0 +1,28 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Map",
+        props: {
+            locations: Array,
+            msg: String,
+            multiply: Function,
+        },
+        data() {
+            return {
+                switchValue: false,
+            }
+        },
+        methods: {
+            handleChange(a) {
+                console.log(a);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
